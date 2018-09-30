0552d9ab4cbe249c4b09241fae1871533c1c9cf8
Tiimor <tiimor@qq.com>
2018-09-30 17:30:03 +0800

    独立依赖包模式

---
+++ AndroidMediaPlayer/src/main/java/com/excellence/android/mediaplayer/AndroidMediaPlayer.java
@@ -0,0 +1,14 @@
+package com.excellence.android.mediaplayer;
+
+import com.excellence.mediaplayer.player.BaseMediaPlayer;
+
+/**
+ * <pre>
+ *     author : VeiZhang
+ *     blog   : http://tiimor.cn
+ *     time   : 2018/9/30
+ *     desc   : 原生播放器
+ * </pre> 
+ */
+public class AndroidMediaPlayer extends BaseMediaPlayer {
+}
--- MediaPlayer/src/main/java/com/excellence/mediaplayer/player/IMediaPlayer.java
+++ MediaPlayer/src/main/java/com/excellence/mediaplayer/player/IMediaPlayer.java
@@ -1,11 +1,11 @@
-package com.excellence.mediaplayer;
+package com.excellence.mediaplayer.player;
 
 /**
  * <pre>
  *     author : VeiZhang
  *     blog   : http://tiimor.cn
  *     time   : 2018/9/30
- *     desc   :
+ *     desc   : 播放器接口
  * </pre> 
  */
 public interface IMediaPlayer {
