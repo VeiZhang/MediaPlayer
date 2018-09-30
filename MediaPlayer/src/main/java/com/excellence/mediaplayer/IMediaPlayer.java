06a9e5923bc6ca1a2292f1892479cf9d65264d91
Tiimor <tiimor@qq.com>
2018-09-30 16:08:07 +0800

    重命名依赖

---
+++ MediaPlayer/src/main/java/com/excellence/mediaplayer/IMediaPlayer.java
@@ -0,0 +1,12 @@
+package com.excellence.mediaplayer;
+
+/**
+ * <pre>
+ *     author : VeiZhang
+ *     blog   : http://tiimor.cn
+ *     time   : 2018/9/30
+ *     desc   :
+ * </pre> 
+ */
+public interface IMediaPlayer {
+}
